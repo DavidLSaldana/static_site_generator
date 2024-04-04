0c011a12cc08b5f7c9c13ea6428c6de657095b44
DavidLSaldana <dleosaldana@gmail.com>
2024-04-03 22:04:55 -0700

    textnode class, main.py, gitignore, main.sh

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode
+
+
+def main():
+    my_node = TextNode("First","Try","TEST.com")
+    print(my_node)
+    return 1
+
+    
+    
+main()
---
+++ src/textnode.py
@@ -0,0 +1,19 @@
+class TextNode():
+    """docstring for TextNode."""
+    def __init__(self, text, text_type, url=None):
+        super(TextNode, self).__init__()
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, text_node):
+        if self.text == text_node.text \
+            and self.text_type == text_node.text_type \
+            and self.url == text_node.url:
+                return True
+        else:
+            return False
+            
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
