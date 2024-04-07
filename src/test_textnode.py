0e0afbb2ed6526a60b555805361af578ae613afa
DavidLSaldana <dleosaldana@gmail.com>
2024-04-07 12:56:47 -0700

    textnodes split based on delimiter and tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,8 @@
 import unittest
 
-from textnode import TextNode
+from textnode import *
+#from textnode import TextNode
+#from textnode import split_nodes_delimiter
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -11,8 +13,17 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(node1, node2)
 
         node3 = TextNode("This is a node", "bold")
-        node4 = TextNode("This is a text node", "Italic")
-        node5 = TextNode("This is a text node", "Italic", "www.mysite.com")
+        node4 = TextNode("This is a text node", "Italic", "www.mysite.com")
+        node5 = TextNode("This is text with a `code block` word", text_type_text)
+        node6 = TextNode("This is text with an error `code block word", text_type_text)
+        new_nodes = split_nodes_delimiter([node5], "`", text_type_code)
+        print(f"No Error: new_nodes {new_nodes}")
+        #new_nodes = split_nodes_delimiter([node6], "`", text_type_code)
+        #print(f"With Error: new_nodes {new_nodes}")
+
+        node7 = TextNode("This is text with a `code block` and an *Italic Word* and a **Bold Word**", text_type_text)
+        new_nodes = split_nodes_delimiter([node7], "**", text_type_code)
+        print(f"No Error: new_nodes {new_nodes}")
 
         #print(f"node3: {node3}")
         #print(f"node4: {node4}")
--- src/textnode.py
+++ src/textnode.py
@@ -7,6 +7,10 @@ text_type_code = "code"
 text_type_link = "link"
 text_type_image = "image"
 
+code_delimit = '`'
+bold_delimit = '**'
+italic_delimit = '*'
+
 class TextNode():
     """docstring for TextNode."""
     def __init__(self, text, text_type, url=None):
@@ -40,3 +44,38 @@ def text_node_to_html_node(text_node):
     if text_node.text_type == text_type_image:
         return LeafNode("img", "", {"src": text_node.link, "alt": text_node.text})
     raise ValueError (f"Invalid text type: {text_node.text_type}")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    #takes list of old nodes, a delimiter, and a text type
+    #returns a new list of nodes where any "text" type are
+    #potentially split based on syntax
+    new_text_type = ""
+    new_list_nodes = []
+    if delimiter == code_delimit:
+        new_text_type = text_type_code
+    if delimiter == italic_delimit:
+        new_text_type = text_type_italic
+    if delimiter == bold_delimit:
+        new_text_type = text_type_bold
+    
+    temp_func = lambda delimit: delimit.text.split(delimiter)
+
+    for node in old_nodes:
+        
+        if not node.text_type == text_type_text:
+            new_list_nodes.append(node)
+        else:
+            working_node = temp_func(node)
+            working_node_length = len(working_node)
+            if working_node_length % 2 == 0:
+                raise Exception("Invalid Markdown Syntax")
+
+            for w_node in range(0, len(working_node)):
+                if w_node %2 == 1:
+                    new_list_nodes.append(TextNode(working_node[w_node],new_text_type,None))
+                else:
+                    new_list_nodes.append(TextNode(working_node[w_node],text_type_text,None))
+
+
+
+    return new_list_nodes
